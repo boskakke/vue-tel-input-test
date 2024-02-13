443aa5c669b5a081bc1bcc3f960028e0778f7f85
Bo Skakke <bo@skakke.dk>
2024-02-13 09:45:05 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { VueTelInput } from 'vue-tel-input'
+import 'vue-tel-input/vue-tel-input.css'
+import { ref, reactive } from 'vue'
+
+const phone = ref(null)
+const bindProps = reactive({
+  mode: 'international',
+
+  name: 'phone',
+  maxLen: 25,
+
+  inputOptions: {
+    required: true,
+    autocomplete: 'off',
+    placeholder: 'Enter a phone number',
+    showDialCode: true
+  }
+})
+</script>
+
+<template>
+  <VueTelInput v-model="phone" v-bind="bindProps"></VueTelInput>
+</template>
